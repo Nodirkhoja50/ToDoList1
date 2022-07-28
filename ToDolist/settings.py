651672033822ec2a513d9ff2a70cd12566411e09
Nodirkhoja Khamitov <1910250.nodirkhoja.khamitov@gmail.com>
2022-07-28 18:39:52 +0500

    added staticfiles

--- ToDolist/settings.py
+++ ToDolist/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,6 +115,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+STATIC_URL = os.path.join(BASE_DIR,'staticfiles')
 
 LOGIN_URL = 'login-user'
 
